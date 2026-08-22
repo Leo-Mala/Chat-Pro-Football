1e062906d67046a50b31ebc928e009a2954a2129
Márcio Leandro de Almeida Malaquias <leomalaj10@gmail.com>
2026-08-22 20:30:09 -0300

    fix: restore correct FontFamily import

--- app/src/main/java/com/example/ui/screens/GameScreens.kt
+++ app/src/main/java/com/example/ui/screens/GameScreens.kt
@@ -39,7 +39,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.FontFamily
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
